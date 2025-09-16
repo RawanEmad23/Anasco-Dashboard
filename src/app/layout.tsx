88f84327bded8c5657b33da6ead3347b648c64fa
RawanEmad23 <re0999578@gmail.com>
2025-09-16 20:48:52 +0300

    update in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Sidebar from "@/components/sidebare"
 import Navbare from "@/components/navbare"
 import Activitypanel from "@/components/activitypanel"
-
 import "./globals.css"
 
 export default function RootLayout({
@@ -13,22 +11,27 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-const [sidebarOpen, setSidebarOpen] = useState(true)
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+  const [isMounted, setIsMounted] = useState(false) 
+
+  useEffect(() => {
+    setIsMounted(true)
+    const isMobile = window.innerWidth < 768
+    setSidebarOpen(!isMobile)
+  }, [])
+
+  if (!isMounted) return null 
 
   return (
     <html lang="en">
       <body className="flex h-screen overflow-hidden bg-gray-50">
-     
         <Sidebar
           open={sidebarOpen}
           onClose={() => setSidebarOpen(false)}
         />
 
         <div className="flex flex-col flex-1 overflow-hidden">
-     
           <Navbare toggleSidebar={() => setSidebarOpen((prev) => !prev)} />
-
-         
           <main className="flex-1 overflow-auto p-4 bg-gray-100">
             {children}
           </main>
